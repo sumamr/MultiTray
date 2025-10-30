3bf682e9011cd0ebc963de84db85f55980ea5f46
sumamr <99878446+sumamr@users.noreply.github.com>
2025-10-30 14:23:14 +0530

    Add files via upload

---
+++ role.guard.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, Router } from '@angular/router';
+import { AuthService } from '../Services/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RoleGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(route: ActivatedRouteSnapshot): boolean {
+    const requiredRole = route.data['role'];
+    const userRole = this.authService.getUserRole();
+
+    if (userRole === requiredRole) {
+      return true;
+    }
+
+    this.router.navigate(['/login']);
+    return false;
+  }
+}
